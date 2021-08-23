e75e8de0cbb2f1d5def2527dd207dd1385f3035c
lean251290 <lean251290@gmail.com>
2021-08-23 09:22:13 -0300

    ya carga la imagen en el servidor

--- php/operaciones/ci_foto.php
+++ php/operaciones/ci_foto.php
@@ -1,6 +1,7 @@
 <?php
 class ci_foto extends leanInicial_ci
 {
+	protected $s__path_inicial;
 	
 
 	//-----------------------------------------------------------------------------------
@@ -9,9 +10,38 @@ class ci_foto extends leanInicial_ci
 
 	function evt__foto_frm__alta($datos)
 	{
-		var_dump($datos);
-		exit();
+		$foto_tmp = "";
+		$imagen_a_tratar = ($datos['foto']);
+		
+		$ruta_final = 'img/imagenes/';
+
+		$ruta_inicial = $this->s__path_inicial; 
+
+		$this->s__path_inicial = toba::proyecto()->get_www($ruta_final);
+
+		if (file_exists($ruta_inicial.$ruta_final)){
+			
+			$nombre_img = basename($datos['foto']['name']);
+			$foto_tmp = $datos['foto']['tmp_name'];
+			
+			if (move_uploaded_file($foto_tmp, $ruta_final . $nombre_img)) {
+				echo 'cargo';
+				return;
+			}else{
+				echo 'no cargo';
+			}
+
+
+		}else{
+			mkdir($ruta_inicial.$ruta_final);
+
+			chmod($ruta_inicial.$ruta_final, 0777);
+
+			$nombre_img = $ruta_inicial . $ruta_final . basename($datos['foto']['name']);
+		}
+		
 	}
 
 }
 ?>
+

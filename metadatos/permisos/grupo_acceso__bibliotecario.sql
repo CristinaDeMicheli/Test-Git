c5ce2f75ad831523560689ff6dd8ad4b52239665
AndreaMonzon <ticmonzonandrea@hotmail.com>
2021-09-15 11:12:44 -0300

    agregue mis cmabios andrea

--- metadatos/permisos/grupo_acceso__bibliotecario.sql
+++ metadatos/permisos/grupo_acceso__bibliotecario.sql
@@ -4,16 +4,16 @@
 ------------------------------------------------------------
 INSERT INTO apex_usuario_grupo_acc (proyecto, usuario_grupo_acc, nombre, nivel_acceso, descripcion, vencimiento, dias, hora_entrada, hora_salida, listar, permite_edicion, menu_usuario) VALUES (
 	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
-	'Administrador', --nombre
-	'0', --nivel_acceso
-	'Accede a toda la funcionalidad', --descripcion
+	'bibliotecario', --usuario_grupo_acc
+	'bibliotecario', --nombre
+	NULL, --nivel_acceso
+	NULL, --descripcion
 	NULL, --vencimiento
 	NULL, --dias
 	NULL, --hora_entrada
 	NULL, --hora_salida
 	NULL, --listar
-	'1', --permite_edicion
+	'0', --permite_edicion
 	NULL  --menu_usuario
 );
 
@@ -24,7 +24,7 @@ INSERT INTO apex_usuario_grupo_acc (proyecto, usuario_grupo_acc, nombre, nivel_a
 --- INICIO Grupo de desarrollo 0
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
+	'bibliotecario', --usuario_grupo_acc
 	NULL, --item_id
 	'2'  --item
 );
@@ -33,115 +33,73 @@ INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, i
 --- INICIO Grupo de desarrollo 262
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
+	'bibliotecario', --usuario_grupo_acc
 	NULL, --item_id
 	'262000030'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
+	'bibliotecario', --usuario_grupo_acc
 	NULL, --item_id
 	'262000031'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
-	NULL, --item_id
-	'262000032'  --item
-);
-INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
-	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
-	NULL, --item_id
-	'262000034'  --item
-);
-INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
-	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
-	NULL, --item_id
-	'262000037'  --item
-);
-INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
-	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
-	NULL, --item_id
-	'262000038'  --item
-);
-INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
-	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
+	'bibliotecario', --usuario_grupo_acc
 	NULL, --item_id
 	'262000040'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
+	'bibliotecario', --usuario_grupo_acc
 	NULL, --item_id
 	'262000042'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
+	'bibliotecario', --usuario_grupo_acc
 	NULL, --item_id
 	'262000043'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
+	'bibliotecario', --usuario_grupo_acc
 	NULL, --item_id
 	'262000044'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
+	'bibliotecario', --usuario_grupo_acc
 	NULL, --item_id
 	'262000045'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
+	'bibliotecario', --usuario_grupo_acc
 	NULL, --item_id
 	'262000046'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
+	'bibliotecario', --usuario_grupo_acc
 	NULL, --item_id
 	'262000047'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
-	NULL, --item_id
-	'262000048'  --item
-);
-INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
-	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
-	NULL, --item_id
-	'262000049'  --item
-);
-INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
-	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
-	NULL, --item_id
-	'262000050'  --item
-);
-INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
-	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
+	'bibliotecario', --usuario_grupo_acc
 	NULL, --item_id
 	'262000051'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
+	'bibliotecario', --usuario_grupo_acc
 	NULL, --item_id
 	'262000052'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'leanInicial', --proyecto
-	'admin', --usuario_grupo_acc
+	'bibliotecario', --usuario_grupo_acc
 	NULL, --item_id
 	'262000053'  --item
 );
